9ee151af3f432741f6c6abebda31e938f062c668
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-04-21 14:06:19 +0200

    With user

---
+++ src/main/java/io/github/oliviercailloux/exercices/car/user/CarUser.java
@@ -0,0 +1,17 @@
+package io.github.oliviercailloux.exercices.car.user;
+
+import io.github.oliviercailloux.exercices.car.ImmutablePerson;
+
+public class CarUser {
+	public static void main(String[] args) {
+		MemorizerCar car = new MemorizerCar(new ImmutablePerson("name", 10));
+		car.drive(1);
+		car.drive(2);
+		car.drive(5);
+		car.getTotalTraveledDistance();
+		// 10
+		car.getFirstPosition();
+		car.getSecondPosition();
+		car.getFirstPosition();
+	}
+}
